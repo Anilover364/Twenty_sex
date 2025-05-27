c2f98cbb20297c168278d6eea6f4c93415d6e81d
Moderator <timurrocev427@gmail.com>
2025-05-27 23:18:20 +0300

    Добавьте файлы проекта.

---
+++ Twenty_sex/MainWindow.xaml.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Twenty_sex
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Square square;
+        Rect rect;
+        RUSSIA RUSSIA;
+        int x, y;
+        bool sq, rc, RF;
+
+        private void Click(object sender, RoutedEventArgs e)
+        {
+            var send=sender as MenuItem;
+            switch (send.Name) 
+            {
+                case "Sqr":
+                    Sqr.IsEnabled = false;
+                    Pen.IsEnabled = true;
+                    RUS.IsEnabled = true;
+                    square.Show();
+                    rect.Hide();
+                    RUSSIA.Hide();
+                    break;
+                case "Pen":
+                    Sqr.IsEnabled = true;
+                    Pen.IsEnabled = false;
+                    RUS.IsEnabled = true;
+                    square.Hide();
+                    rect.Show();
+                    RUSSIA.Hide();
+                    break;
+                case "RUS":
+                    Sqr.IsEnabled = true;
+                    Pen.IsEnabled = true;
+                    RUS.IsEnabled = false;
+                    square.Hide();
+                    rect.Hide();
+                    RUSSIA.Show();
+                    break;
+            }
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            square = new Square(x,y,Polotno);
+            //Square(0, 0, Polotno);.
+            square.Show();
+            rect = new Rect(x, y, Polotno);
+            rect.Hide();
+            //rect.Show();
+            RUSSIA = new RUSSIA(x, y, Polotno);
+            RUSSIA.Hide();
+            //RUSSIA.Show();
+          
+        }
+
+        private void Window_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.Key)
+            {
+                case Key.D:
+                    x = x + 5;
+                    
+                    break;
+                case Key.A:
+                    x = x - 5;
+                    
+                    break;
+                case Key.W:
+                    y = y - 5;
+                    
+                    break;
+                case Key.S:
+                    y = y + 5;
+                   
+                    break;
+            }
+            square.MoveXY(x, y);
+            rect.MoveXY(x, y);  
+            RUSSIA.MoveXY(x, y);
+            //switch (sq,rc,RF) 
+            //{
+            //    case (true,false,false):
+            //        break;
+            //}
+        }
+    }
+}
---
+++ Twenty_sex/Square.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace Twenty_sex
+{
+    internal class Square
+    {
+        protected int _x, _y,_side1;
+        protected Brush _stroke;
+        protected GeometryGroup _group;
+        protected System.Windows.Shapes.Path _path;
+        protected RectangleGeometry _square;
+        public Brush Stroke 
+        {
+            get { return _stroke; }
+            set { _stroke = value; _path.Stroke = _stroke; }
+        }
+        public int x => _x;
+        public int y => _y;
+        public int side1 => _side1;
+        public Square(int x, int y, Canvas canvas)
+        {
+            _x = x;
+            _y = y;
+            _side1 = 150;
+            _stroke = Brushes.Black;
+            _path = new System.Windows.Shapes.Path();
+            _square = new RectangleGeometry();
+            _group = new GeometryGroup();
+            _square.Rect = new System.Windows.Rect(0, 0, _side1, _side1);
+            _path.Stroke = _stroke;
+            _group.Children.Add(_square);
+            _path.Data = _group;
+            canvas.Children.Add(_path);
+            Canvas.SetLeft(_path, _x);
+            Canvas.SetTop(_path, _y);
+        }
+        public void SetLoc() 
+        {
+            Canvas.SetLeft(_path, _x);
+            Canvas.SetTop(_path, _y);
+        }
+        public void Show()
+        {
+            _path.Visibility = Visibility.Visible;
+        }
+        public void Hide()
+        {
+            _path.Visibility = Visibility.Hidden;
+        }
+        public void MoveXY(int x, int y)
+        {
+            _x = x; _y = y;
+            SetLoc();
+        }
+    }
+    class Rect : Square 
+    {
+
+        public Rect(int x, int y, Canvas canvas) : base(x, y, canvas) 
+        {
+        _square.Rect=new System.Windows.Rect(0,0,_side1*2,side1);
+
+        }
+       
+    }
+    class RUSSIA : Rect 
+    {
+        //RectangleGeometry _rectBlue;
+        //RectangleGeometry _rectRed;
+
+  
+        public RUSSIA(int x, int y, Canvas canvas) : base(x, y, canvas) 
+        {
+            //_rectBlue = new RectangleGeometry();
+            //_rectRed = new RectangleGeometry();
+            //_rectBlue.Rect = new System.Windows.Rect(0, 50, _side1 * 2, side1-100);
+           
+            //_group.Children.Add(_rectBlue);
+            //_rectRed.Rect = new System.Windows.Rect(0, 100, _side1 * 2, side1 - 100);
+            //_group.Children.Add(_rectRed);
+
+            _path.Fill = new LinearGradientBrush(
+        new GradientStopCollection
+        {
+            new GradientStop(Colors.White, 0.0),
+            new GradientStop(Colors.Blue, 0.33),
+            new GradientStop(Colors.Red, 0.66)
+        },
+        startPoint: new Point(0, 0),
+        endPoint: new Point(0, 1));
+        }
+       
+    }
+}
